992285beb52ab0ee4b821e67261f40783485adad
Lukasdv11 <lukas.dvs02@gmail.com>
2025-10-21 22:02:56 -0300

    2nd day exercises

--- Semana 1/Dia1.py
+++ Semana 1/Dia1.py
@@ -1,8 +1,9 @@
 print("Hola usuario.")
 
-# nombre = input("Cual es tu nombre usuario?: ")
-# print(f"Bienvenido a la Red {nombre}")
+nombre = input("Cual es tu nombre usuario?: ")
+print(f"Bienvenido a la Red {nombre}")
 
 A = int(input("Ingresa un numero: "))
 B = int(input("Ingresa otro numero: "))
 print(f"La suma es: {A + B}.")
+
