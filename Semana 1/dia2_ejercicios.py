4be40f2c8dc8f2f06068b6ded526620fb1533d31
Lukasdv11 <lukas.dvs02@gmail.com>
2025-10-21 22:10:17 -0300

    Ahora si?

--- Semana 1/dia2_ejercicios.py
+++ Semana 1/dia2_ejercicios.py
@@ -1,43 +1,43 @@
 #Number verification.
-# num1 = float(input("Type a number: "))
+num1 = float(input("Type a number: "))
 
-# if num1 > 0:
-#     print("Your number is positive!.")
-# elif num1 == 0:
-#     print("Your number is zero!.")
-# else:
-#     print("Your number is negative!.")
+if num1 > 0:
+    print("Your number is positive!.")
+elif num1 == 0:
+    print("Your number is zero!.")
+else:
+    print("Your number is negative!.")
     
     
 #Age verification.
-# age = int(input("Type your age: "))
+age = int(input("Type your age: "))
 
-# if age < 0:
-#     print("Type a valid age.")
-# elif age < 18:
-#     print("You are under age 18.")
-# elif age >= 18:
-#     print("You are over 18 years.")
+if age < 0:
+    print("Type a valid age.")
+elif age < 18:
+    print("You are under age 18.")
+elif age >= 18:
+    print("You are over 18 years.")
 
-# Pair or impair number.
+#Pair or impair number.
 
-# num2 = int(input("Type a number: "))
+num2 = int(input("Type a number: "))
     
-# if num2 % 2:
-#     print("Your number is impair!.")
-# else:
-#     print("Your number is pair!.")
+if num2 % 2:
+    print("Your number is impair!.")
+else:
+    print("Your number is pair!.")
 
 #Approved and reproved grades.
-# grade = float(input("Type your grade (1.0 to 7.0): "))
+grade = float(input("Type your grade (1.0 to 7.0): "))
 
-# if grade < 1:
-#     print("Type a valid grade (1.0 to 7.0).")
-# elif grade >7:
-#     print("Type a valid grade (1.0 to 7.0).")
-# elif grade < 4:
-#     print("You are reproved.")
-# elif grade >= 4:
-#     print("You are APPROVED!.")
+if grade < 1:
+    print("Type a valid grade (1.0 to 7.0).")
+elif grade >7:
+    print("Type a valid grade (1.0 to 7.0).")
+elif grade < 4:
+    print("You are reproved.")
+elif grade >= 4:
+    print("You are APPROVED!.")
 
 
